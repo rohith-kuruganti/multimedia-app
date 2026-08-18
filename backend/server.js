adf7c4507ee7b1d905a6100e779c19c064846600
rohith-kuruganti <rohithreddykuruganti@gmail.com>
2026-08-18 20:14:44 +0530

    conect backend to mongoDB

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ const express = require("express");
 
 const config = require("./config/env");
 const healthRoutes = require("./routes/health.routes");
+const connectDB = require("./config/db");
 
 const app = express();
 
@@ -9,6 +10,7 @@ app.use(express.json());
 
 app.use("/api/health", healthRoutes);
 
+connectDB();
 app.listen(config.port, () => {
   console.log(`Server running on port ${config.port}`);
 });
